cb4ad195847e815f5f3c054b66c0e7a09ab764b3
Harsheek Thanki <harsheekthanki@gmail.com>
2016-02-11 00:02:12 +0000

    Add carousel to homepage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Nomster::Application.configure 
 
-ActionMailer::Base.smtp_settings = {
+AActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
